d56655200cb50dbf2057bdee324280f6398b6cc4
王俊雄 <tsvsfcu21695@gmail.com>
2017-12-25 06:27:34 +0800

    12-25 購物車雛形

--- CowBaby/src/main/java/model/dao/impl/ProductDaoImpl.java
+++ CowBaby/src/main/java/model/dao/impl/ProductDaoImpl.java
@@ -10,11 +10,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import model.bean.ProductBean;
 import model.dao.ProductDao;
 
 @Repository
+@Transactional
 public class ProductDaoImpl implements ProductDao {
 	@Autowired
 	private SessionFactory sessionFactory;
